5b7ecd8d219f50fd00cfab8212ef9b0fc23fca2b
soohmatthew <matthewsoh18@gmail.com>
2020-04-06 17:46:50 +0800

    Added grayscale flag

--- utils/data.py
+++ utils/data.py
@@ -38,13 +38,14 @@ def rle_to_mask(rle_string, width, height):
         return img
 
 class Dataset(data.Dataset):
-    def __init__(self, image_filepath, EncodedPixels, size, transforms = None, test = False, equalise = True, label = ['fish', 'flower', 'gravel', 'sugar'], data_augmentations = None):
+    def __init__(self, image_filepath, EncodedPixels, size, transforms = None, test = False, equalise = True, label = ['fish', 'flower', 'gravel', 'sugar'], data_augmentations = None, grayscale = False):
         self.image_filepath = image_filepath
         self.EncodedPixels = EncodedPixels # Each encodedpixels should be (4, H, W) for 4 masks, in the order of Fish, Flower, Gravel and Sugar masks
         self.transforms = transforms
         self.test = test
         self.size = size
         self.equalise = equalise
+        self.grayscale = grayscale
         self.label = label
         self.list_of_classes = ['fish', 'flower', 'gravel', 'sugar']
         self.data_augmentations = data_augmentations
@@ -54,7 +55,7 @@ class Dataset(data.Dataset):
     def __getitem__(self, index):
         ID = self.image_filepath[index]
         # Load data and get label
-        X = histogram_equalize(ID, self.equalise)
+        X = histogram_equalize(ID, self.equalise, self.grayscale)
         if self.transforms is not None:
             X = self.transforms(X)
         if self.test:
@@ -88,20 +89,24 @@ class Dataset(data.Dataset):
             masks = torch.stack(masks_to_include)
             return X, masks
 
-def histogram_equalize(filepath, equalise = False):
+def histogram_equalize(filepath, equalise = False, grayscale = False):
+    if grayscale:
+        img = cv2.imread(filepath, 0)
+        ret, thresh1 = cv2.threshold(img, 50, 255, cv2.THRESH_TOZERO)
+        img = cv2.cvtColor(thresh1,cv2.COLOR_GRAY2RGB)
+    else:
+        img = cv2.imread(filepath, 1)
+
     if equalise:
          # read a image using imread 
-        img = cv2.imread(filepath, 0)
-        ret,thresh1 = cv2.threshold(img, 50, 255, cv2.THRESH_TOZERO)
-        thresh1 = cv2.cvtColor(thresh1,cv2.COLOR_GRAY2RGB)
-        b,g,r = cv2.split(thresh1)
+        b,g,r = cv2.split(img)
         equ_b = cv2.equalizeHist(b)
         equ_g = cv2.equalizeHist(g)
         equ_r = cv2.equalizeHist(r)
         equ = cv2.merge((equ_b, equ_g, equ_r))
         img = Image.fromarray(equ)
     else:
-        img = Image.open(filepath)
+        img = Image.fromarray(img)
     return img
 
 def split_data(df_filepath, seed, train_proportion = 0.9):
@@ -124,14 +129,14 @@ def group_data(df, image_filepath):
     masks = [encodedpixels[i:i+4] for i in range(0, len(encodedpixels), 4)]
     return images, masks
 
-def prepare_dataloader(train_image_filepath, test_image_filepath, df_filepath, seed, train_transform, test_transform, size, batch_size = 1, shuffle_val_dataloader = False, label = None, data_augmentations = None):
+def prepare_dataloader(train_image_filepath, test_image_filepath, df_filepath, seed, train_transform, test_transform, size, batch_size = 1, shuffle_val_dataloader = False, label = None, data_augmentations = None, grayscale = False):
     train_df, valid_df = split_data(df_filepath, seed)
     train_images, train_masks = group_data(train_df, train_image_filepath)
     valid_images, valid_masks = group_data(valid_df, train_image_filepath)
     test_images = [test_image_filepath + '/' + i for i in os.listdir(test_image_filepath)]
-    train_ds = Dataset(train_images, train_masks, size = size, transforms = train_transform, label = label, data_augmentations = data_augmentations)
-    valid_ds = Dataset(valid_images, valid_masks, size = size, transforms = test_transform, label = label)
-    test_ds = Dataset(test_images, EncodedPixels = None, transforms = test_transform,  size = size, test = True, label = label)
+    train_ds = Dataset(train_images, train_masks, size = size, transforms = train_transform, label = label, data_augmentations = data_augmentations, grayscale = grayscale)
+    valid_ds = Dataset(valid_images, valid_masks, size = size, transforms = test_transform, label = label, grayscale = grayscale)
+    test_ds = Dataset(test_images, EncodedPixels = None, transforms = test_transform,  size = size, test = True, label = label, grayscale = grayscale)
 
     train_dl = data.DataLoader(train_ds, batch_size = batch_size, shuffle = True)
     valid_dl = data.DataLoader(valid_ds, batch_size = batch_size, shuffle = shuffle_val_dataloader)
@@ -160,4 +165,4 @@ if __name__ == "__main__":
     data_augmentations = [torchvision.transforms.RandomHorizontalFlip(p= 1), 
                           torchvision.transforms.RandomVerticalFlip(p= 1)]
 
-    train_dl, valid_dl, test_dl = prepare_dataloader(train_image_filepath, test_image_filepath, df_filepath, seed, train_transform, test_transform, 256, 64, label = ['fish'], data_augmentations = data_augmentations)
+    train_dl, valid_dl, test_dl = prepare_dataloader(train_image_filepath, test_image_filepath, df_filepath, seed, train_transform, test_transform, 256, 64, label = ['fish'], data_augmentations = data_augmentations, grayscale = True)
