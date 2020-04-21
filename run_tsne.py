8f0dfbf6f8270fb24b7fe4f22c8804da9c55f6e6
soohmatthew <matthewsoh18@gmail.com>
2020-04-21 10:22:40 +0800

    Update tsne

--- run_tsne.py
+++ run_tsne.py
@@ -71,44 +71,45 @@ if __name__ == '__main__':
                 x = X_embedded[:,0]
                 y = X_embedded[:,1]
                 outliers = np.where( (x > np.percentile(x, 99)) | (x < np.percentile(x,1)) | (y > np.percentile(x, 99)) | (y < np.percentile(x,1)) )
+                print(outliers)
                 for idx in range(len(list(outliers[0]))):
-                    counter += 1
                     if counter >= 25:
                         break
                     ax2[counter//5,counter % 5].imshow(orig_img[list(outliers[0])[idx]].permute(1, 2, 0).cpu().detach().numpy())
+                    counter += 1
                 ax = fig.add_subplot(2, 3, i+1)
                 ax.scatter(x, y, alpha = 0.3)
                 ax.set_title(f'Perplexity: {perplexities[i]}')
             fig.savefig(f'intraclass_{classes}.png')
             fig2.savefig(f'outliers_{classes}.png')
 
-    # Inter-class tsne
-    batch_size = int(batch_size/4)
-    inter_class_img = []
-    col_labels = []
-    col_dict = {'flower': 'blue', 'fish' : 'green', 'gravel' : 'black', 'sugar' : 'pink'}
-    for lab in ['flower', 'fish', 'sugar', 'gravel']:
-        autoencoder = torch.load(os.path.join(os.getcwd(),'weights',f'{classes}_Autoencodercurrent_model.pth'), map_location = device)
-        autoencoder.eval()
-        with torch.no_grad():
-            for _, data in enumerate(train_dataloader):
-                x, _ = data
-                x = x.to(device)
-                _ = autoencoder.forward(x)
-                latent = autoencoder.latent.detach().cpu()
-                inter_class_img.append(latent)
-                col_labels += [col_dict[lab] for i in range(latent.shape[0])]
-                break
-    inter_class_img_stack = torch.cat(inter_class_img)
-    perplexities = np.arange(5, 60, 10)
-    fig = plt.figure(figsize=(10,10), dpi= 100)
-    fig.suptitle('Inter-class t-sne')
-    for i in range(len(perplexities)):
-        X_embedded = TSNE(n_components=2, perplexity = perplexities[i]).fit_transform(inter_class_img_stack)
-        print(f'TSNE built for inter-class, perplexity: {perplexities[i]}')
-        x = X_embedded[:,0]
-        y = X_embedded[:,1]
-        ax = fig.add_subplot(2, 3, i+1)
-        ax.scatter(x, y, c = col_labels, alpha = 0.3)
-        ax.set_title(f'Perplexity: {perplexities[i]}')
-    fig.savefig(f'interclass.png')
+    # # Inter-class tsne
+    # batch_size = int(batch_size/4)
+    # inter_class_img = []
+    # col_labels = []
+    # col_dict = {'flower': 'blue', 'fish' : 'green', 'gravel' : 'black', 'sugar' : 'pink'}
+    # for lab in ['flower', 'fish', 'sugar', 'gravel']:
+    #     autoencoder = torch.load(os.path.join(os.getcwd(),'weights',f'{classes}_Autoencodercurrent_model.pth'), map_location = device)
+    #     autoencoder.eval()
+    #     with torch.no_grad():
+    #         for _, data, orig_img in enumerate(train_dataloader):
+    #             x, _ = data
+    #             x = x.to(device)
+    #             _ = autoencoder.forward(x)
+    #             latent = autoencoder.latent.detach().cpu()
+    #             inter_class_img.append(latent)
+    #             col_labels += [col_dict[lab] for i in range(latent.shape[0])]
+    #             break
+    # inter_class_img_stack = torch.cat(inter_class_img)
+    # perplexities = np.arange(5, 60, 10)
+    # fig = plt.figure(figsize=(10,10), dpi= 100)
+    # fig.suptitle('Inter-class t-sne')
+    # for i in range(len(perplexities)):
+    #     X_embedded = TSNE(n_components=2, perplexity = perplexities[i]).fit_transform(inter_class_img_stack)
+    #     print(f'TSNE built for inter-class, perplexity: {perplexities[i]}')
+    #     x = X_embedded[:,0]
+    #     y = X_embedded[:,1]
+    #     ax = fig.add_subplot(2, 3, i+1)
+    #     ax.scatter(x, y, c = col_labels, alpha = 0.3)
+    #     ax.set_title(f'Perplexity: {perplexities[i]}')
+    # fig.savefig(f'interclass.png')
